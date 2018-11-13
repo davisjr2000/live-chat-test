eb0d7dff86f9fde85c246eda360892719fa6727c
Davis JR <davisjr2000@hotmail.com>
2018-11-13 19:09:08 -0200

    Chat working :)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/create'
+  get 'chat_rooms/show'
   devise_for :users
   root to: 'pages#home'
+  resources :chat_rooms, only: [:show] do
+    resources :messages, only: [:create]
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
