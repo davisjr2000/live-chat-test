fb1550f56843f214c75a67c81604af2b24703755
Davis JR <davisjr2000@hotmail.com>
2018-11-15 10:58:05 -0200

    texto

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :messages, only: [:create]
   end
   resources :users, only: [:index, :show]
+  mount ActionCable.server => "/cable"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
