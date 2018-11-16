ab1dfc4d5215c2832c6818e199dfdf9b7a011270
Henrique Aguilar <henriaguilar@gmail.com>
2018-11-16 15:38:35 -0200

    working on Agenda Views

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -6,6 +6,17 @@ class AgendasController < ApplicationController
   end
 
   def create
+    @user = current_user
+    @sensei = Sensei.find(params[:sensei_id])
+    @agenda = Agenda.new
+    @agenda.sensei = @sensei
+    @agenda.date = params[:agenda][:date]
+    @agenda.time = params[:agenda][:time]
+    if @agenda.save
+      render :new
+    else
+      raise
+    end
   end
 
   def edit
