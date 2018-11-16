4bce31e7e202d3ab19cae76b641451ae85e925d6
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-16 00:01:57 -0300

    working on the files views that needs to be worked

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'lesson_request/new'
-  get 'lesson_request/create'
-  get 'lesson_request/show'
-  get 'lesson_request/index'
-  get 'lesson_request/destroy'
-  get 'sensei_subject/new'
-  get 'sensei_subject/create'
-  get 'sensei_subject/index'
-  get 'sensei_subject/destroy'
-  get 'subject/index'
-  get 'agenda/new'
-  get 'agenda/create'
-  get 'agenda/edit'
-  get 'agenda/update'
-  get 'agenda/index'
-  get 'agenda/destroy'
-  get 'sensei/new'
-  get 'sensei/create'
-  get 'sensei/index'
-  get 'sensei/show'
+
   devise_for :users
   root to: 'pages#home'
   resources :chat_rooms, only: [:show, :index] do
