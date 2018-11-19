7b2e03bfd9842eb95c2a19c5f28f64e751eb753d
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-19 10:22:04 -0300

    putting adm setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
   resources :chat_rooms, only: [:show, :index] do
