c9a39df28c9fca33e7518288d1851b12720be01c
Davis JR <davisjr2000@hotmail.com>
2018-11-19 21:12:15 -0200

    Better UX

--- app/models/sensei.rb
+++ app/models/sensei.rb
@@ -4,9 +4,9 @@ class Sensei < ApplicationRecord
   has_many :subjects, through: :sensei_subjects
   validates :user_id, uniqueness: true, presence: true
   include PgSearch
-  pg_search_scope :search_by_username,
+  pg_search_scope :search_by_subject,
     associated_against: {
-      sensei_subjects: [ :subject_id ]
+      subjects: [ :title ]
     },
     using: {
       tsearch: { prefix: true }
