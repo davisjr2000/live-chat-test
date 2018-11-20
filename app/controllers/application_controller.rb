d5e2cfededb37b7bf5a136a9398fbd41ab09cdfd
THIAGO SANTOS <thiagosantosengcivil@gmail.com>
2018-11-20 14:55:27 -0200

    Social Meta Tags Setup in Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,3 +11,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :photo, :description])
   end
 end
+
+def default_url_options
+  { host: ENV["HOST"] || "localhost:3000" }
+end
